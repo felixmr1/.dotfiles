b7c9aae84d0650fa76e3d73438afc2dfda1e4960
Felix Rosén <felix.rosen@einride.tech>
2022-08-20 11:16:11 +0200

    revert nvim

---
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -0,0 +1,8 @@
+local colorscheme = "catppuccin"
+
+vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
