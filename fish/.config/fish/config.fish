ec2855d8f4ac42e051d486c486cb2052e8db07ba
Felix Rosén <felix.rosen@einride.tech>
2022-08-30 09:21:01 +0200

    add fish bass with gvm and gcloud

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,5 +6,11 @@ function fish_user_key_bindings
   fish_vi_key_bindings
 end
 
-alias gs="git status"
+# The next line updates PATH for the Google Cloud SDK.
+bass source '/home/felix/.local/share/google-cloud-sdk/path.bash.inc'
 
+# The next line enables shell command completion for gcloud.
+bass source '/home/felix/.local/share/google-cloud-sdk/completion.bash.inc'
+
+alias gs="git status"
+alias einride="cd /home/felix/Dev/einride/"
