2e3d3ebe6587b62b0ffdcf46f171258281def69f
felixmr1 <felix.rosen@einride.tech>
2025-12-17 16:31:56 +0100

    fix(home): disable catppuccin nvim integration

--- home/common/catppuccin.nix
+++ home/common/catppuccin.nix
@@ -4,6 +4,10 @@
   catppuccin = {
     enable = true;
     flavor = "mocha";
+
+    # Only enable for fish
+    # nvim is managed separately via symlink to dotfiles
     fish.enable = true;
+    nvim.enable = false;
   };
 }
